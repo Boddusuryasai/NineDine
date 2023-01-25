d2a9ced54e86c9c8389ae5e9184484dcb6f7d30d
suryasai <suryasaichanti@gmail.com>
2023-01-25 15:28:43 +0530

    routing added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,46 @@
 import React from "react";
 import  ReactDOM  from "react-dom/client";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import About from "./Components/About";
 import Navbar from "./Components/Navbar";
 import RestaurantList from "./Components/RestaurantList";
+import RestaurantMenu from "./Components/RestaurantMenu";
+import Cart from "./Components/Cart"
 
 const Container =()=>{
-   return (
-     <>
-       <Navbar />
-       <RestaurantList/>
-     </>
-   );
+  return (
+    <>
+      <Navbar />
+      <Outlet/>
+    </>
+  );
 } 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Container/>,
+    children: [
+      {
+      path:"/",
+      element: <RestaurantList/>
+      },
+      {
+        path:"/about",
+        element: <About/>
+        },
+        {
+          path:"/cart",
+          element: <Cart/>
+          },
+          {
+            path:"/restaurantinfo/:id",
+            element: <RestaurantMenu/>
+            }
+    ]
+  }
+]);
+
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(<Container/>)
+root.render(<RouterProvider router={router} />)
