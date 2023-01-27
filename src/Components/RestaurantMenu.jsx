170322c6bad3ece81d1f33dd25e1041cb58a2120
suryasai <suryasaichanti@gmail.com>
2023-01-27 18:37:29 +0530

    UI changed for menu

--- src/Components/RestaurantMenu.jsx
+++ src/Components/RestaurantMenu.jsx
@@ -58,27 +58,20 @@ function restaurantInfo() {
         </div>
       </div>
 
-      <div className="flex flex-col justify-center items-center gap-2 mt-2">
+      <div className="flex flex-row flex-wrap justify-center items-center gap-4 mt-2">
         {filteredrestaurantMenu.map((item) => (
-          <div
-            key={item.id}
-            className="flex flex-row justify-between items-center w-3/6 border p-2 shadow-xl"
-          >
-            <div>
-              <h3 className="font-bold font-[Poppins]">{item.name}</h3>
-              <h3 className="text-sm  font-[Poppins]">₹{item.price / 100}</h3>
-            </div>
-            <div className="flex flex-col justify-center items-center gap-2">
-              <img
-                className="h-28 rounded-lg"
-                src={`https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/${item.cloudinaryImageId}`}
-                alt=""
-              />
-              <button
-                type="button"
-                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-              >
-                <svg
+          
+           <div class="relative w-[300px] mt-4" key={item.id}>
+          <img class="w-full h-[300px] rounded-md"  src={`https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/${item.cloudinaryImageId}`}
+                alt=""/>
+          <div class="w-[280px] absolute border shadow-lg left-[50%] bottom-[-5%]  bg-white/50  p-4 rounded-xl translate-x-[-50%] ">
+              <div class="flex flex-row  justify-between items-center gap-3">
+              <div>
+                  <h4 class="text-black text-xl font-bold">{item.name}</h4>
+                  <small class="text-black font-semibold">₹{item.price / 100}</small>
+              </div>
+         
+              <button  className="text-white bg-[#171A29]  focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2 "><svg
                   aria-hidden="true"
                   className="w-5 h-5 mr-2 -ml-1"
                   fill="currentColor"
@@ -86,12 +79,12 @@ function restaurantInfo() {
                   xmlns="http://www.w3.org/2000/svg"
                 >
                   <path d="M3 1a1 1 0 000 2h1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43-.893.892C3.74 11.846 4.632 14 6.414 14H15a1 1 0 000-2H6.414l1-1H14a1 1 0 00.894-.553l3-6A1 1 0 0017 3H6.28l-.31-1.243A1 1 0 005 1H3zM16 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z"></path>
-                </svg>
-                Add to Cart
-              </button>
-            </div>
+                </svg>Add</button>
           </div>
-        ))}
+          </div>
+          </div>))}
+          
+        
       </div>
     </div>
   );
