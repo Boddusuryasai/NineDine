12213910d8209492eb1b8180a711ef7cfcfbd26c
suryasai <suryasaichanti@gmail.com>
2023-01-27 19:04:57 +0530

    Nabar toggle after clicking routes improved

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -64,7 +64,7 @@ const Navbar = () => {
               className={`fixed w-full  duration-300 bg-[#171A29] h-screen top-[68px] md:hidden flex flex-col p-4 mt-4
           ${toggle ? "left-0" : "left-[-100%]"}`}
             >
-              <li>
+              <li  onClick={() => setToggle(!toggle)}>
                 <Link
                   to="/"
                   className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
@@ -73,7 +73,7 @@ const Navbar = () => {
                   Home
                 </Link>
               </li>
-              <li>
+              <li  onClick={() => setToggle(!toggle)}>
                 <Link
                   to="/about"
                   className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
@@ -81,7 +81,7 @@ const Navbar = () => {
                   About
                 </Link>
               </li>
-              <li>
+              <li  onClick={() => setToggle(!toggle)}>
                 <Link
                   to="/cart"
                   className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
